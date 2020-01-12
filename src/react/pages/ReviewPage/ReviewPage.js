048f43a0dabef2ebe7b970db7c377c33cb52e357
Christian Rojas <cjrojas72@gmail.com>
2020-01-12 01:52:54 -0600

    basic layout for page

--- src/react/pages/ReviewPage/ReviewPage.js
+++ src/react/pages/ReviewPage/ReviewPage.js
@@ -1,15 +1,64 @@
 import React from "react";
 import { Menu } from "../../components";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Table } from "react-bootstrap";
+import {
+  Table,
+  InputGroup,
+  FormControl,
+  Button,
+  Container,
+  Row,
+  Col,
+  Dropdown,
+  DropdownButton
+} from "react-bootstrap";
 import "./ReviewPage.css";
 
 class ReviewPage extends React.Component {
   render() {
     return (
-      <div>
+      <Container>
         <Menu />
         <h1>Review Page</h1>
+        <Row>
+          <Col xs={3}>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Search Keyword"
+                aria-label="Search Keyword"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="primary">Search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Col>
+          <Col>
+            <DropdownButton
+              id="dropdown-basic-button"
+              title="Filter by Category"
+              variant="outline-secondary"
+            >
+              <Dropdown.Item href="#/action-1">Category 1</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Category 2</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Category 3</Dropdown.Item>
+            </DropdownButton>
+          </Col>
+          <Col>
+            <Button variant="primary">Show All</Button>
+          </Col>
+          <Col>
+            <DropdownButton
+              id="dropdown-basic-button"
+              title="Results per Page"
+              variant="outline-secondary"
+            >
+              <Dropdown.Item href="#/action-1">Category 1</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Category 2</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Category 3</Dropdown.Item>
+            </DropdownButton>
+          </Col>
+        </Row>
         <Table responsive striped bordered hover className="table-width">
           <thead>
             <tr>
@@ -64,7 +113,7 @@ class ReviewPage extends React.Component {
             </tr>
           </tbody>
         </Table>
-      </div>
+      </Container>
     );
   }
 }
