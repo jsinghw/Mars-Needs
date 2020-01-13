d01d479fd88026323ccf760f606b097a5f5ff387
Christian Rojas <cjrojas72@gmail.com>
2020-01-12 23:47:02 -0600

    update to component library and sort func for table

--- src/react/pages/ReviewPage/ReviewPage.js
+++ src/react/pages/ReviewPage/ReviewPage.js
@@ -1,118 +1,110 @@
 import React from "react";
 import { Menu } from "../../components";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Table,
-  InputGroup,
-  FormControl,
-  Button,
-  Container,
-  Row,
-  Col,
-  Dropdown,
-  DropdownButton
-} from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { MDBDataTable } from "mdbreact";
 import "./ReviewPage.css";
 
 class ReviewPage extends React.Component {
   render() {
+    const data = {
+      columns: [
+        {
+          label: "Number",
+          field: "number",
+          sort: "asc"
+        },
+        {
+          label: "Title",
+          field: "title",
+          sort: "asc"
+        },
+        {
+          label: "Category",
+          field: "category",
+          sort: "asc"
+        },
+        {
+          label: "Submission Date",
+          field: "submissionDate",
+          sort: "asc"
+        }
+      ],
+      rows: [
+        {
+          number: "AA493892B2",
+          title: "Red Planet Donuts",
+          category: "Food & Drink",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA493t422B2",
+          title: "Space Shoes",
+          category: "Clothing",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA4952242B2",
+          title: "Hover Board",
+          category: "Vechicles",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA49535B2",
+          title: "Wall-E",
+          category: "Robotics",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA493231122B2",
+          title: "Ray Gun",
+          category: "Weapons",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA42332292B2",
+          title: "Mars Tents",
+          category: "Housing",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA4554592B2",
+          title: "Mars Cookies",
+          category: "Food & Drink",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA49366667B2",
+          title: "Lightsaber",
+          category: "Weapons",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA493822TRF2",
+          title: "Solar Panels",
+          category: "Energy",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA493JGJD453",
+          title: "Stuf and things",
+          category: "Misc",
+          submissionDate: "2020-01-07"
+        },
+        {
+          number: "AA49GJ452GG2",
+          title: "Mars Soap",
+          category: "Personal Care",
+          submissionDate: "2020-01-07"
+        }
+      ]
+    };
+
     return (
       <Container>
         <Menu />
         <h1>Review Page</h1>
-        <Row>
-          <Col xs={3}>
-            <InputGroup className="mb-3">
-              <FormControl
-                placeholder="Search Keyword"
-                aria-label="Search Keyword"
-                aria-describedby="basic-addon2"
-              />
-              <InputGroup.Append>
-                <Button variant="primary">Search</Button>
-              </InputGroup.Append>
-            </InputGroup>
-          </Col>
-          <Col>
-            <DropdownButton
-              id="dropdown-basic-button"
-              title="Filter by Category"
-              variant="outline-secondary"
-            >
-              <Dropdown.Item href="#/action-1">Category 1</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Category 2</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Category 3</Dropdown.Item>
-            </DropdownButton>
-          </Col>
-          <Col>
-            <Button variant="primary">Show All</Button>
-          </Col>
-          <Col>
-            <DropdownButton
-              id="dropdown-basic-button"
-              title="Results per Page"
-              variant="outline-secondary"
-            >
-              <Dropdown.Item href="#/action-1">Category 1</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Category 2</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Category 3</Dropdown.Item>
-            </DropdownButton>
-          </Col>
-        </Row>
-        <Table responsive striped bordered hover className="table-width">
-          <thead>
-            <tr>
-              <th>Proposal ID</th>
-              <th>Title</th>
-              <th>Category</th>
-              <th>Submission Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-              <td>Table cell</td>
-            </tr>
-          </tbody>
-        </Table>
+        <MDBDataTable striped bordered hover data={data} />
       </Container>
     );
   }
