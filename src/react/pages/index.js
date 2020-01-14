f5bf2bd6f6c0c2a9de6c55ca0ae4ba41dd7f84d1
Christian Rojas <cjrojas72@gmail.com>
2020-01-14 12:35:08 -0600

    resolve merge conflicts

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -1,18 +1,11 @@
 import Home from "./Home/Home";
-<<<<<<< HEAD
 import ReviewPage from "./ReviewPage/ReviewPage";
 import NotFound from "./NotFound";
-
-export default {
-  Home: { path: "/", component: Home },
-  ReviewPage: { path: "/reviewpage", component: ReviewPage },
-=======
-import NotFound from "./NotFound"
-import Proposal from "./Proposal/Proposal"
+import Proposal from "./Proposal/Proposal";
 
 export default {
   Home: { path: "/", component: Home },
   Proposal: { path: "/proposal", component: Proposal },
->>>>>>> 992fb648db4d6434b62e220c493b34860e79b048
+  ReviewPage: { path: "/reviewpage", component: ReviewPage },
   NotFound: { path: "*", component: NotFound }
 };
