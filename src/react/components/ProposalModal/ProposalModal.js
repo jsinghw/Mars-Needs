8e00a8f259c9dd0a36d39f31224b636697079c85
Christian Rojas <cjrojas72@gmail.com>
2020-01-15 00:14:13 -0600

    saving changes

--- src/react/components/ProposalModal/ProposalModal.js
+++ src/react/components/ProposalModal/ProposalModal.js
@@ -1,12 +1,6 @@
 import React from "react";
-import {
-  MDBContainer,
-  MDBBtn,
-  MDBModal,
-  MDBModalBody,
-  MDBModalHeader,
-  MDBModalFooter
-} from "mdbreact";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import "./ProposalModal.css";
 
 class ProposalModal extends React.Component {
@@ -16,21 +10,30 @@ class ProposalModal extends React.Component {
     }
     return (
       <>
-        <div>
-          <h1>hello</h1>
-        </div>
-        <div className="modalDiv" id="modalDiv">
-          <h2>Modal Window</h2>
-          <div className="content">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
-            deserunt corrupti, ut fugit magni qui quasi nisi amet repellendus
-            non fuga omnis a sed impedit explicabo accusantium nihil doloremque
-            consequuntur.
-          </div>
-          <div className="actions">
-            <button className="toggle-button">OK</button>
-          </div>
-        </div>
+        <Modal size="lg" show={this.props.show}>
+          <Modal.Header>
+            <Modal.Title>{this.props.number}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Title: {this.props.title}</p>
+            <p>Submission Date: {this.props.submissionDate}</p>
+            <p>Category: {this.props.category}</p>
+            <p>
+              Details: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.toggle}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </>
     );
   }
