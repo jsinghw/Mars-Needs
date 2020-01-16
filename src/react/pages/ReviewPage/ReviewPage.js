2fb807cdbf4b8798dcc6de339450f5bf4790d200
Christian Rojas <cjrojas72@gmail.com>
2020-01-15 20:44:06 -0600

    data populates rows in table

--- src/react/pages/ReviewPage/ReviewPage.js
+++ src/react/pages/ReviewPage/ReviewPage.js
@@ -5,11 +5,35 @@ import { Container } from "react-bootstrap";
 import { MDBDataTable, MDBBtn } from "mdbreact";
 import { Modal } from "../../components";
 import "./ReviewPage.css";
+import axios from "axios";
 
 class ReviewPage extends React.Component {
   state = {
-    show: false
+    show: false,
+    rows: [],
+    name: "",
+    email: "",
+    proposal_title: "",
+    proposal_category: "",
+    proposal_details: "",
+    company: "",
+    industry: "",
+    phone_number: "",
+    website: ""
   };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8082/api/proposals")
+      .then(response => {
+        console.log(response);
+        this.setState({ rows: response.data });
+        console.log(this.state.rows);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
   toggle = () => {
     this.setState({
@@ -17,15 +41,8 @@ class ReviewPage extends React.Component {
     });
   };
 
-  // setData = () => {
-  //   this.setState({
-  //     button: this.button
-  //   });
-
-  //   console.log(this.state.button);
-  // };
-
   render() {
+    console.log(this.state.rows);
     const data = {
       columns: [
         {
@@ -53,138 +70,18 @@ class ReviewPage extends React.Component {
           field: "file"
         }
       ],
-      rows: [
-        {
-          number: "AA493892B2",
-          title: "Red Planet Donuts",
-          category: "Food & Drink",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn
-              button="0"
-              className="purple"
-              outline
-              size="sm"
-              onClick={e => {
-                this.toggle();
-              }}
-            >
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA493t422B2",
-          title: "Space Shoes",
-          category: "Clothing",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm">
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA4952242B2",
-          title: "Hover Board",
-          category: "Vechicles",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA49535B2",
-          title: "Wall-E",
-          category: "Robotics",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File <Modal />
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA493231122B2",
-          title: "Ray Gun",
-          category: "Weapons",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA42332292B2",
-          title: "Mars Tents",
-          category: "Housing",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA4554592B2",
-          title: "Mars Cookies",
-          category: "Food & Drink",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA49366667B2",
-          title: "Lightsaber",
-          category: "Weapons",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA493822TRF2",
-          title: "Solar Panels",
-          category: "Energy",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA493JGJD453",
-          title: "Stuf and things",
-          category: "Misc",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        },
-        {
-          number: "AA49GJ452GG2",
-          title: "Mars Soap",
-          category: "Personal Care",
-          submissionDate: "2020-01-07",
-          file: (
-            <MDBBtn className="purple" outline size="sm" onClick={this.toggle}>
-              File
-            </MDBBtn>
-          )
-        }
-      ]
+      rows: []
     };
+
+    this.state.rows.map(function(row, i) {
+      console.log(row);
+      return data.rows.push({
+        number: row._id,
+        title: row.proposal_title,
+        category: row.proposal_category,
+        submissionDate: "need a date"
+      });
+    });
 
     return (
       <Container>
